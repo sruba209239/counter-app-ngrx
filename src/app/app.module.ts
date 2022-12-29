2e249d9c28fcb4879b76110942ea7c2726fa7948
sruba mohapatra <sruba209239@gmail.com>
2022-12-29 21:05:33 +0530

    added routing and posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/state/counter.reducer';
 import { CustomCounterStepInputComponent } from './counter/custom-counter-step-input/custom-counter-step-input.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
@@ -17,13 +22,18 @@ import { FormsModule } from '@angular/forms';
     CounterOutputComponent,
     CounterControlsComponent,
     CustomCounterStepInputComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostsListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    StoreModule.forRoot({ counter: counterReducer })
+    StoreModule.forRoot({ counter: counterReducer }),
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
